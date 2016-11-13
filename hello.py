83675563b723c7444421d17555e2487963c485bc
keren <dkeren@seas.upenn.edu>
2016-11-13 09:10:18 -0500

    descriptions

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, request, redirect, url_for
 from flask import send_from_directory
 import mahotas as mh
@@ -9,6 +10,7 @@ from werkzeug.utils import secure_filename
 UPLOAD_FOLDER = './'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 SIGMA = 16.5
+SIGMA2 = 12
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def allowed_file(filename):
@@ -28,6 +30,9 @@ def upload_file():
         file = request.files['file']
         sigma = request.form['sig']
         SIGMA = sigma
+        sigma2 = request.form['sig2']
+        SIGMA2 = sigma2
+        print sigma2
         # if user does not select file, browser also
         # submit a empty part without filename
         if file.filename == '':
@@ -42,12 +47,12 @@ def upload_file():
         f = f0[:,:,0]
         #the first gaussian determines the blur
         #the higher the sigma, the blurier the image
-        f2 = mh.gaussian_filter(f, 12)
+        f2 = mh.gaussian_filter(f, int(SIGMA2))
         f = (f2 < f2.mean())
         labeled, total  = mh.label(f)
         imshow(labeled)
         #lower the sigma, the more local min it finds
-        dnaf = mh.gaussian_filter(f2, SIGMA)
+        dnaf = mh.gaussian_filter(f2, int(SIGMA))
         rmax = mh.regmin(dnaf)
         seeds, final_total = mh.label(rmax)
         title("Microscopic Organisms Found:{}".format(int(final_total)))
@@ -77,11 +82,20 @@ def upload_file():
     <h1>BioCounter</h1>
     <p>Counting microorganisms is a tedious task that biologist have.
     BioCounter allows you to count them instantly.<p>
+    <p>The way it works:<p>
+    <p>BioCounter treats the image as an array. It first performs a gaussian filter 
+    to separate the background from the objects. Sigma1 determines the stardard deviation 
+    of the gaussian.In simpler terms, it increase the blur in image. 
+    Next, it performs a second gaussian filter to determine the local min int the image to 
+    account for merged cells. The Sigma2 will affect the number of local min the image finds.
     <table>
     <th><form action="" method=post enctype=multipart/form-data></th>
     <tr><td> <h3>Select New File: <input type=file name=file></h3></td></tr>
     <tr><td>
-    <h3>Select Sigma: <input type=number name=sig></h3>
+    <h3>Select Sigma1: <input type=number name=sig2></h3>
+    </td></tr>
+    <tr><td>
+    <h3>Select Sigma2: <input type=number name=sig></h3>
     </td></tr>
     <tr><td>
     <input type=submit value=Submit>
