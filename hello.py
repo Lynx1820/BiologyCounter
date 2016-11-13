ebf2f37fedd8c84384e60541ad7f7c7b8d38200d
keren <dkeren@seas.upenn.edu>
2016-11-13 05:24:41 -0500

    basic functions work

--- hello.py
+++ hello.py
@@ -2,13 +2,13 @@ from flask import Flask, request, redirect, url_for
 from flask import send_from_directory
 import mahotas as mh
 import numpy as np
-from pylab import imshow, gray, show, jet
+from pylab import imshow, gray, show, jet, title
 import os 
 from werkzeug.utils import secure_filename
 #upload folder is where you store the files
 UPLOAD_FOLDER = './'
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
-
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+SIGMA = 16.5
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def allowed_file(filename):
@@ -16,23 +16,7 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
-	f1 = mh.imread(filename)
-	f0 = f1
-	f = f0[:,:,0]
-	f2 = mh.gaussian_filter(f, 8)
-	f = (f2 < f2.mean())
-	imshow(f)
-	labeled, n_nucleus  = mh.label(f)
-	print('Found {} nuclei.'.format(n_nucleus))
-	imshow(labeled)
-	print f2.shape
-	dnaf = mh.gaussian_filter(f2, 16.5)
-	rmax = mh.regmin(dnaf)
-	imshow(mh.overlay(f2, rmax))
-	show()
-	seeds,nr_nuclei = mh.label(rmax)
-	print nr_nuclei
-    	return send_from_directory(app.config['UPLOAD_FOLDER'],
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
                                filename)
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
@@ -42,6 +26,8 @@ def upload_file():
             flash('No file part')
             return redirect(request.url)
         file = request.files['file']
+        sigma = request.form['sig']
+        SIGMA = sigma
         # if user does not select file, browser also
         # submit a empty part without filename
         if file.filename == '':
@@ -51,19 +37,59 @@ def upload_file():
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             #we redirect the user to url_for('uploaded_file', filename=filename)
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
+        f1 = mh.imread(filename)
+        f0 = f1
+        f = f0[:,:,0]
+        #the first gaussian determines the blur
+        #the higher the sigma, the blurier the image
+        f2 = mh.gaussian_filter(f, 12)
+        f = (f2 < f2.mean())
+        labeled, total  = mh.label(f)
+        imshow(labeled)
+        #lower the sigma, the more local min it finds
+        dnaf = mh.gaussian_filter(f2, SIGMA)
+        rmax = mh.regmin(dnaf)
+        seeds, final_total = mh.label(rmax)
+        title("Microscopic Organisms Found:{}".format(int(final_total)))
+        imshow(mh.overlay(f2, rmax))
+        show()
     return '''
     <!doctype html>
+    <html>
     <title>The Biology Counter</title>
-    <h1>The Biology Counter</h1>
-    <p1>Counting microscopic organisms is a tedious task that all biologist have.
-    With The Biology Counter, you can count them instantly<p1>
-    <h2>Upload New File<h2>
-    <form action="" method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-         <input type=submit value=Upload>
+    <style>
+    body {
+    background-color: #f1ead5;
+    margin-left: 100px;
+    }
+    h1,input,h3{
+    color: #347e91;
+    margin-left: 100px;
+    }
+
+    p, p1{
+    color: #347e91;
+    margin-left: 100px;
+    }
+
+    </style>
+    <body>
+    <h1>BioCounter</h1>
+    <p>Counting microorganisms is a tedious task that biologist have.
+    BioCounter allows you to count them instantly.<p>
+    <table>
+    <th><form action="" method=post enctype=multipart/form-data></th>
+    <tr><td> <h3>Select New File: <input type=file name=file></h3></td></tr>
+    <tr><td>
+    <h3>Select Sigma: <input type=number name=sig></h3>
+    </td></tr>
+    <tr><td>
+    <input type=submit value=Submit>
+    </td></tr>
+    </table>  
     </form>
+    <body>
+    </html>
     '''
 if __name__ == '__main__':
-   app.run(debug = True)
+    app.run(debug = True)
